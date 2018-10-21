0067bc9c220b79efc33480aa2801ecf330af3b00
saad3344555 <saad_makani@hotmail.com>
2018-10-21 18:32:35 +0500

    bugs fixed till face detection and registeration

--- app/src/main/java/com/webxert/studentapp_ip_fyp/MainActivity.java
+++ app/src/main/java/com/webxert/studentapp_ip_fyp/MainActivity.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.microsoft.projectoxford.face.FaceServiceClient;
 import com.microsoft.projectoxford.face.FaceServiceRestClient;
 import com.microsoft.projectoxford.face.contract.AddPersistedFaceResult;
 import com.microsoft.projectoxford.face.contract.CreatePersonResult;
+import com.microsoft.projectoxford.face.contract.Person;
 import com.microsoft.projectoxford.face.contract.PersonGroup;
 import com.microsoft.projectoxford.face.contract.TrainingStatus;
 import com.microsoft.projectoxford.face.rest.ClientException;
@@ -23,16 +25,90 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FaceServiceRestClient faceServiceRestClient = new FaceServiceRestClient("https://westcentralus.api.cognitive.microsoft.com/face/v1.0", "fb5b93f74a464f19992f53dce8e24af0");
+    private FaceServiceRestClient faceServiceRestClient = new FaceServiceRestClient("https://westcentralus.api.cognitive.microsoft.com/face/v1.0", "8b505b08755d46cd9aa514f6a8d7ab17");
 
     ProgressDialog dialog;
     PersonGroup group;
 
-    Bitmap bitmap;
     ByteArrayInputStream byteArrayInputStream;
 
     Bitmap[] bitmaps = new Bitmap[3];
 
+    final AsyncTask<Void, String, CreatePersonResult> createPersonResult = new AsyncTask<Void, String, CreatePersonResult>() {
+
+        @Override
+        protected void onPreExecute() {
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(CreatePersonResult createPersonResult) {
+            dialog.dismiss();
+            Log.e("personId", String.valueOf(createPersonResult.personId));
+            if (createPersonResult != null) {
+
+                new DetectAndRegister(createPersonResult).execute();
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            publishProgress(values[0]);
+        }
+
+        @Override
+        protected CreatePersonResult doInBackground(Void... voids) {
+            try {
+                publishProgress("Creating person in group");
+                dialog.dismiss();
+                return faceServiceRestClient.createPerson("xyz", "Zayn", null);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                dialog.dismiss();
+                return null;
+            }
+        }
+    };
+
+
+    final AsyncTask<Void, String, Boolean> createPersonGroup = new AsyncTask<Void, String, Boolean>() {
+        @Override
+        protected void onProgressUpdate(String... values) {
+            dialog.setMessage(values[0]);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                //updating old group. If you want to create a new group change to createPerson Group....
+                faceServiceRestClient.updatePersonGroup("xyz", "UBIT students", null);
+            } catch (Exception e) {
+                Log.e("Client/IO Exception", e.getMessage());
+                publishProgress("Exception caught");
+                e.printStackTrace();
+                dialog.dismiss();
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            dialog.dismiss();
+            try {
+                getPersonResult.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,83 +119,9 @@ public class MainActivity extends AppCompatActivity {
         //here create function which returns bitmap
 
         bitmaps[0] = BitmapFactory.decodeResource(getResources(), R.drawable.zayn);
-        bitmaps[1] = BitmapFactory.decodeResource(getResources(), R.drawable.zayn2);
-        bitmaps[2] = BitmapFactory.decodeResource(getResources(), R.drawable.zayn3);
+        bitmaps[1] = BitmapFactory.decodeResource(getResources(), R.drawable.zayn_2);
+        bitmaps[2] = BitmapFactory.decodeResource(getResources(), R.drawable.zayn_3);
 
-
-        final AsyncTask<Void, String, CreatePersonResult> createPersonResult = new AsyncTask<Void, String, CreatePersonResult>() {
-
-            @Override
-            protected void onPostExecute(CreatePersonResult createPersonResult) {
-                Log.e("personId", String.valueOf(createPersonResult.personId));
-                if (createPersonResult != null) {
-
-                    new DetectAndRegister(createPersonResult).execute();
-                }
-            }
-
-            @Override
-            protected void onProgressUpdate(String... values) {
-                publishProgress(values[0]);
-            }
-
-            @Override
-            protected CreatePersonResult doInBackground(Void... voids) {
-                try {
-                    publishProgress("Creating person in group");
-                    return faceServiceRestClient.createPerson("students", "shariq", null);
-
-                } catch (Exception e) {
-                    Log.e("ExceptionCreatingPerson", "EXCEPTION");
-                    return null;
-                }
-            }
-        };
-
-
-        final AsyncTask<Void, String, Boolean> createPersonGroup = new AsyncTask<Void, String, Boolean>() {
-            @Override
-            protected void onProgressUpdate(String... values) {
-                dialog.setMessage(values[0]);
-            }
-
-            @Override
-            protected void onPreExecute() {
-                dialog.show();
-            }
-
-            @Override
-            protected Boolean doInBackground(Void... voids) {
-                try {
-                    faceServiceRestClient.createPersonGroup("students", "UBIT students", null);
-                } catch (Exception e) {
-                    Log.e("Client/IO Exception", e.getMessage());
-                    publishProgress("Exception caught");
-                    dialog.dismiss();
-                    return false;
-                }
-
-                return true;
-            }
-
-            @Override
-            protected void onPostExecute(Boolean bool) {
-                dialog.dismiss();
-                try {
-                    group = faceServiceRestClient.getPersonGroup("students");
-                    if (group != null) {
-                        Log.e("personCreated", String.valueOf(bool));
-                        if (bool)
-                            createPersonResult.execute();
-
-                    }
-
-
-                } catch (Exception e) {
-                    Log.e("GettingGroupException", e.getMessage());
-                }
-            }
-        };
 
         findViewById(R.id.press_me).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,12 +143,20 @@ public class MainActivity extends AppCompatActivity {
 
         CreatePersonResult createPersonResult;
 
+
+        @Override
+        protected void onPreExecute() {
+            dialog.show();
+        }
+
         public DetectAndRegister(CreatePersonResult createPersonResult) {
             this.createPersonResult = createPersonResult;
         }
 
         @Override
         protected Boolean doInBackground(InputStream... inputStreams) {
+
+            dialog.dismiss();
 
             //creating function which returns compressed bitmap
 
@@ -164,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             try {
-                                AddPersistedFaceResult faceResult = faceServiceRestClient.addPersonFace("students", createPersonResult.personId, byteArrayInputStream, null, null);
+                                AddPersistedFaceResult faceResult = faceServiceRestClient.addPersonFace("xyz", createPersonResult.personId, byteArrayInputStream, null, null);
                                 if (faceResult != null) {
                                     Log.e("persistedFaceId", String.valueOf(faceResult.persistedFaceId));
 
@@ -177,12 +187,15 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                     thread.start();
+
+                    byteArrayOutputStream.close();
+                    byteArrayInputStream.close();
                 }
             } catch (Exception e) {
                 Log.e("AddingFaceException", e.getMessage());
+                e.printStackTrace();
                 return false;
             }
-
 
             return true;
         }
@@ -197,6 +210,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    final AsyncTask<Void,Void,Boolean> getPersonResult =  new AsyncTask<Void,Void,Boolean>(){
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                group = faceServiceRestClient.getPersonGroup("xyz");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            if (group != null) {
+                Log.e("personCreated", String.valueOf(bool));
+                if (bool) {
+                    createPersonResult.execute();
+                }
+            }
+        }
+    };
+
     private class TrainPersonGroup extends AsyncTask<Void, Void, Void> {
 
         @Override
@@ -204,11 +242,11 @@ public class MainActivity extends AppCompatActivity {
 
 
             try {
-                faceServiceRestClient.trainPersonGroup("students");
+                faceServiceRestClient.trainPersonGroup("xyz");
                 TrainingStatus trainingStatus = null;
 
                 while (true) {
-                    trainingStatus = faceServiceRestClient.getPersonGroupTrainingStatus("students");
+                    trainingStatus = faceServiceRestClient.getPersonGroupTrainingStatus("xyz");
                     if (trainingStatus.status != TrainingStatus.Status.Running)
                         break;
                     Thread.sleep(1000);
@@ -217,7 +255,8 @@ public class MainActivity extends AppCompatActivity {
 
 
             } catch (Exception e) {
-                Log.e("TrainingException", e.getMessage());
+                Log.d("Training Error",e.getMessage());
+                e.printStackTrace();
             }
 
             return null;
